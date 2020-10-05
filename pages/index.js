ec9a8e8ebc40801ecc99baee72e921c321bbff7a
Hemedi Saidi <hemedi@protonmail.com>
2020-10-05 16:31:26 +1030

    Add meta tags

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,51 @@ export default function Home() {
       style={{ gridTemplateRows: "1fr 100px" }}
     >
       <Head>
-        <title>Convert App by Project Groceries</title>
+        {/* Primary Meta Tags */}
+        <title>
+          Convert - A conversion tool for common ingredient measurements
+        </title>
         <link rel="icon" href="/favicon.svg" />
+        <meta
+          name="title"
+          content="Convert - A conversion tool for common ingredient measurements"
+        />
+        <meta
+          name="description"
+          content="Brought to you by the folks at Project Groceries, this converter is a handy tool for whenever you are cooking or just looking to convert ingredient units."
+        />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://projectgroceries.com/" />
+        <meta
+          property="og:title"
+          content="Convert - A conversion tool for common ingredient measurements"
+        />
+        <meta
+          property="og:description"
+          content="Brought to you by the folks at Project Groceries, this converter is a handy tool for whenever you are cooking or just looking to convert ingredient units."
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/projectgroceries/image/upload/v1599364438/Project_Groceries_Card_Image_jjm3gs.png"
+        />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://projectgroceries.com/" />
+        <meta
+          property="twitter:title"
+          content="Convert - A conversion tool for common ingredient measurements"
+        />
+        <meta
+          property="twitter:description"
+          content="Brought to you by the folks at Project Groceries, this converter is a handy tool for whenever you are cooking or just looking to convert ingredient units."
+        />
+        <meta
+          property="twitter:image"
+          content="https://res.cloudinary.com/projectgroceries/image/upload/v1599364438/Project_Groceries_Card_Image_jjm3gs.png"
+        ></meta>
       </Head>
 
       <main className="flex flex-col items-center justify-center">
