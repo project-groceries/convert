6de1b8b05b59cfcb2088e9500d7778d86e5b3c8f
regirock365 <hemedi@protonmail.com>
2022-06-14 15:45:37 +0930

    Update to match rebrand

--- components/Meta.js
+++ components/Meta.js
@@ -18,7 +18,7 @@ const Meta = () => (
 
     {/* Open Graph / Facebook */}
     <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://convert.projectgroceries.com/" />
+    <meta property="og:url" content="https://convert.class-cart.com/" />
     <meta
       property="og:title"
       content="Convert - A conversion tool for common ingredient measurements"
@@ -29,15 +29,12 @@ const Meta = () => (
     />
     <meta
       property="og:image"
-      content="https://res.cloudinary.com/projectgroceries/image/upload/v1601877136/Convert_Meta_Image_xzs3x2.png"
+      content="https://res.cloudinary.com/projectgroceries/image/upload/v1601877136/Convert_Meta_Image_siit36.png"
     />
 
     {/* Twitter */}
     <meta property="twitter:card" content="summary_large_image" />
-    <meta
-      property="twitter:url"
-      content="https://convert.projectgroceries.com/"
-    />
+    <meta property="twitter:url" content="https://convert.class-cart.com/" />
     <meta
       property="twitter:title"
       content="Convert - A conversion tool for common ingredient measurements"
@@ -48,7 +45,7 @@ const Meta = () => (
     />
     <meta
       property="twitter:image"
-      content="https://res.cloudinary.com/projectgroceries/image/upload/v1601877136/Convert_Meta_Image_xzs3x2.png"
+      content="https://res.cloudinary.com/projectgroceries/image/upload/v1601877136/Convert_Meta_Image_siit36.png"
     ></meta>
   </Head>
 );
